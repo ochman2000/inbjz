8d01d185f22c178f4c99e75054f94dd8867cf2e6
Lukasz Ochmanski <ochman2000@gmail.com>
2015-01-08 01:03:31 +0100

    index.html is done

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -11,7 +11,6 @@ public class GreetingController {
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
-        Thread.sleep(3000); // simulated delay
         return new Greeting("Hello, " + message.getName() + "!");
     }
 }
